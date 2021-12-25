65c3cc2d05f667e6d9c76443ffd6670d10c1f47c
= <yura9989@gmail.com>
2021-12-25 22:54:50 +0200

    add main path

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8080;
+
+
+app.use(express.static(path.resolve(__dirname,'client')));
+
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname,'client','index.html;'));
+});
+
+app.listen(port, ()=>{
+    console.log(`Server run on port ${port}`);
+});
